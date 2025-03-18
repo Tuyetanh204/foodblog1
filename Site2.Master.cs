42f086223caf37f840264b9b524959102f6c2c2c
HàThư <haathu1908@gmail.com>
2025-03-18 20:34:29 +0700

    ok

--- Site2.Master.cs
+++ Site2.Master.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -13,31 +11,52 @@ namespace foodblog1
         {
             if (!IsPostBack)
             {
-                // Kiểm tra trạng thái đăng nhập
-                if (Session["Username"] != null) // Người dùng đã đăng nhập
-                {
-                
-                        // Đã đăng nhập
-                        phNavLinks.Controls.Add(new Literal { Text = "<a href='Home.aspx'>Trang chủ</a>" });
-                        phNavLinks.Controls.Add(new Literal { Text = "<div class='dropdown'><button class='dropbtn' disabled>Cá nhân</button><div class='dropdown-content'><a href='new.aspx'>Tạo bài</a><a href='manage.aspx'>Quản lý</a><a href='#' onclick='Logout_Click()'>Đăng xuất</a></div></div>" });
-                    
-
-                }
-                else
-                {
-                    // Chưa đăng nhập
-                    phNavLinks.Controls.Add(new Literal { Text = "<a href='Home.aspx'>Trang chủ</a>" });
-                    phNavLinks.Controls.Add(new Literal { Text = "<a href='register.aspx'>Đăng ký</a>" });
-                    phNavLinks.Controls.Add(new Literal { Text = "<a href='Login.aspx'>Đăng nhập</a>" });
-                }
+               update();
             }
         }
+
         protected void Logout_Click(object sender, EventArgs e)
         {
             // Xóa Session khi người dùng đăng xuất
             Session.Clear();
             Session.Abandon();
-            Response.Redirect("Home.aspx");
+            Response.Redirect("Home.aspx"); // Điều hướng về trang chủ
+            update();
+        }
+        protected void update()
+        {
+            // Thêm nút mặc định luôn hiển thị, như "Trang chủ"
+            phNavLinks.Controls.Add(new Literal { Text = "<a href='Home.aspx'>Trang chủ</a>" });
+
+            if (Session["Username"] != null) // Người dùng đã đăng nhập
+            {
+                // Thêm các liên kết dành cho người dùng đã đăng nhập
+                phNavLinks.Controls.Add(new Literal
+                {
+                    Text = "<div class='dropdown'><button class='dropbtn' disabled>Cá nhân</button><div class='dropdown-content'>"
+                });
+                phNavLinks.Controls.Add(new Literal { Text = "<a href='new.aspx'>Tạo bài</a>" });
+                phNavLinks.Controls.Add(new Literal { Text = "<a href='manage.aspx'>Quản lý</a>" });
+
+                // Tạo nút "Đăng xuất" sử dụng LinkButton
+                LinkButton logoutButton = new LinkButton
+                {
+                    ID = "btnLogout",
+                    Text = "Đăng xuất",
+                    CssClass = "logout-link" // Giữ định dạng giao diện qua CSS
+                };
+                logoutButton.Click += Logout_Click; // Gắn sự kiện Logout_Click
+                phNavLinks.Controls.Add(logoutButton);
+
+                phNavLinks.Controls.Add(new Literal { Text = "</div></div>" });
+            }
+            else
+            {
+                // Thêm các liên kết dành cho người dùng chưa đăng nhập
+                phNavLinks.Controls.Add(new Literal { Text = "<a href='register.aspx'>Đăng ký</a>" });
+                phNavLinks.Controls.Add(new Literal { Text = "<a href='Login.aspx'>Đăng nhập</a>" });
+            }
         }
     }
-}
+    }
+
